14eef394f16501cf9e6f36cf7dc25c813d190f0a
bwyss <bmwyss@gmail.com>
2011-05-10 14:24:13 +0200

    change the urls.py path

--- faultedearth/observations/urls.py
+++ faultedearth/observations/urls.py
@@ -4,6 +4,6 @@ from geonode.observations.views import obs_form_slip, obs_form_displacement, obs
 urlpatterns = patterns('geonode.observations.views',
     #('^obs_form_slip/$', obs_form_slip),
     #('^obs_form_displacement/$', obs_form_displacement),
-    (r'^obsform/$', obsform),
+    url(r'^obsform/$', obsform),
     #(r'^admin/', include(admin.site.urls)),
 )

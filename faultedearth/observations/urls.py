1e634684d84e3ec8cac7af17a7637431b58f9597
bwyss <bmwyss@gmail.com>
2011-05-10 14:46:31 +0200

    rename app to geonode

--- faultedearth/observations/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import *
-from geonode.observations.views import obs_form_slip, obs_form_displacement, obsform
-
-urlpatterns = patterns('geonode.observations.views',
-    #('^obs_form_slip/$', obs_form_slip),
-    #('^obs_form_displacement/$', obs_form_displacement),
-    url(r'^obsform/$', obsform),
-    #(r'^admin/', include(admin.site.urls)),
-)
--- faultedearth/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import *
-from geonode.observations.views import obs_form_slip, obs_form_displacement, obsform
-
-urlpatterns = patterns('',
-    ('^obs_form_slip/$', obs_form_slip),
-    ('^obs_form_displacement/$', obs_form_displacement),
-    ('^obsform/$', obsform),
-    #(r'^admin/', include(admin.site.urls)),
-)

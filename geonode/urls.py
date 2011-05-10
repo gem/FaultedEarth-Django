7694257a8d4d5a65c9c51ae1d2d0f2fb757b4fa9
bwyss <bmwyss@gmail.com>
2011-05-10 14:48:34 +0200

    rename app to geonode

---
+++ geonode/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls.defaults import *
+from geonode.observations.views import obs_form_slip, obs_form_displacement, obsform
+
+urlpatterns = patterns('',
+    ('^obs_form_slip/$', obs_form_slip),
+    ('^obs_form_displacement/$', obs_form_displacement),
+    ('^obsform/$', obsform),
+    #(r'^admin/', include(admin.site.urls)),
+)

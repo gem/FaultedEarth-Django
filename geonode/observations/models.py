3ff898e35bca7b5de728e36a80bc1900c87c1a54
bwyss <bmwyss@gmail.com>
2011-05-13 17:32:27 +0200

    small edit to the tempalte

--- geonode/observations/models.py
+++ geonode/observations/models.py
@@ -12,20 +12,27 @@ class Observations(models.Model):
         ('3','Seismogenic Geometry'),
         ('4','SlipRate'),
     )
-    observationType = models.CharField(max_length=1, choices=OBS_TYPE, default='4')
+    observationType = models.CharField(max_length=1, choices=OBS_TYPE, 
+    default='4')
     SLIP_TYPE = (
         ('0','Normal'),
     )
     slipType = models.CharField(max_length=1, choices=SLIP_TYPE, default='0')
-    hv_ratio = models.CharField(max_length=100, default='1:3.75')
-    rake = models.CharField(max_length=100)
-    net_slip_rate_min = models.CharField(max_length=100, default='min')
-    net_slip_rate_max = models.CharField(max_length=100, default='max')
-    net_slip_rate_pref = models.CharField(max_length=100, default='pref')
-    dip_slip_rate_min = models.CharField(max_length=100, default='min')
-    dip_slip_rate_max = models.CharField(max_length=100, default='max')
-    dip_slip_rate_pref = models.CharField(max_length=100, default='pref')
-    marker_age = models.CharField(max_length=100)
+    hv_ratio = models.CharField(max_length=100, default='1:3.75', blank=True)
+    rake = models.CharField(max_length=100, blank=True)
+    net_slip_rate_min = models.CharField(max_length=100, default='min', 
+    blank=True)
+    net_slip_rate_max = models.CharField(max_length=100, default='max', 
+    blank=True)
+    net_slip_rate_pref = models.CharField(max_length=100, default='pref', 
+    blank=True)
+    dip_slip_rate_min = models.CharField(max_length=100, default='min', 
+    blank=True)
+    dip_slip_rate_max = models.CharField(max_length=100, default='max', 
+    blank=True)
+    dip_slip_rate_pref = models.CharField(max_length=100, default='pref', 
+    blank=True)
+    marker_age = models.CharField(max_length=100, blank=True)
     SLIP_RATE_CAT = (
         ('0','0.001 <0.01'),
         ('1','0.01 <0.1'),
@@ -36,14 +43,22 @@ class Observations(models.Model):
         ('6','50 <100'),
         ('7','100 <200'),
     )
-    slip_rate_category = models.CharField(max_length=10, choices=SLIP_RATE_CAT, default='0')
-    strike_slip_rate_min = models.CharField(max_length=100, default='min')
-    strike_slip_rate_max = models.CharField(max_length=100, default='max')
-    strike_slip_rate_pref = models.CharField(max_length=100, default='pref')
-    vertical_slip_rate_min = models.CharField(max_length=100, default='min')
-    vertical_slip_rate_max = models.CharField(max_length=100, default='max')
-    vertical_slip_rate_pref = models.CharField(max_length=100, default='pref')
-    site = models.CharField(max_length=100, default='-42.375, 176.543')
-    notes = models.TextField()
+    slip_rate_category = models.CharField(max_length=10, choices=SLIP_RATE_CAT, 
+    default='0', blank=True)
+    strike_slip_rate_min = models.CharField(max_length=100, default='min', 
+    blank=True)
+    strike_slip_rate_max = models.CharField(max_length=100, default='max', 
+    blank=True)
+    strike_slip_rate_pref = models.CharField(max_length=100, default='pref', 
+    blank=True)
+    vertical_slip_rate_min = models.CharField(max_length=100, default='min', 
+    blank=True)
+    vertical_slip_rate_max = models.CharField(max_length=100, default='max', 
+    blank=True)
+    vertical_slip_rate_pref = models.CharField(max_length=100, default='pref', 
+    blank=True)
+    site = models.CharField(max_length=100, default='-42.375, 176.543', 
+    blank=True)
+    notes = models.TextField(blank=True)
     class Meta:
         db_table = 'gem\".\"observations_observations'

b956643cda62da47a43d01a31e3b8ee1237c5fa1
Marco Milanesi <marcom@openquake.org>
2011-05-30 13:35:02 +0200

    sanitized urls for production, rough implementation, but probably ok for the demo

--- geonode/urls.py
+++ geonode/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('geonode.observations.views',
-    (r'edit/(?P<observation_id>\d+)/summary_id/(?P<summary_id>\d+)$', 'edit'),
-    (r'new/summary_id/(?P<summary_id>\d+)$', 'new'),
-    (r'^$', 'obsform'),
+   (r'^obsform/edit/(?P<observation_id>\d+)/summary_id/(?P<summary_id>\d+)$',
+        'edit'),
+   (r'^obsform/new/summary_id/(?P<summary_id>\d+)$',
+        'new'),
 )
